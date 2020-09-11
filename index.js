8d0e6755749aebeaadb8ca34a88675839bb71ecd
Luke Thinnes <lukethinnes@yahoo.com>
2020-09-11 01:34:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return `Happy holidays, ${name}`;
+  return `Happy holidays, ${name}!`;
 }
 
 function happyCustomHolidayTo(holiday, name) {
-  return "Happy " + (holiday) + ", " + (name) + "!";
+  return `Happy ${holida}, ${name}!`;
 }
 
 function holidayCountdown(holiday, days) {
